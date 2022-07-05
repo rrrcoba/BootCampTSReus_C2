338e39e6a22f206b49b905cdde7146ce100568cd
roger <roger@DESKTOP-6BBSUVU>
2022-07-05 18:06:22 +0200

    Commit for the first exercice of TA04 of C2.

---
+++ BCTSRS-C1-TA04.1/src/exercice1.java
@@ -0,0 +1,36 @@
+/**
+ * Exercice 1 of the 4th task of the TSystems Bootcamp
+ * 
+ * @author Roger Rubio Rufaste
+ *
+ */
+public class exercice1 {
+	
+	/**
+	 * The main method adds, subtracts, multiplies, divides and makes the module of the two created variables.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//First we declare and initialize the two variables
+		int x = 10;
+		int y = 5;
+		
+		//Then we do the operations
+		//First we add them
+		System.out.println("The result of 10+5 is: "+(x+y));
+		
+		//Then we substract them
+		System.out.println("The result of 10-5 is: "+(x-y));
+		
+		//Then we multiply them
+		System.out.println("The result of 10*5 is: "+(x*y));
+		
+		//Then we divide them
+		System.out.println("The result of 10/5 is: "+(x/y));
+		
+		//Then we do the module of them
+		System.out.println("The result of 10%5 is: "+(x%y));
+	}
+
+}
